529f07e50defe5b63d33d39833d1bf2c5ba436de
Rishabh <rishabh_banga@yahoo.in>
2017-03-14 07:02:18 +0530

    Updated App to support latest builToolsVersion

---
+++ app/src/test/java/rishabhbanga/umeedngo/MainActivityTest.java
@@ -0,0 +1,32 @@
+package rishabhbanga.umeedngo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by erishba on 2/8/2017.
+ */
+public class MainActivityTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onTouchEvent() throws Exception {
+
+    }
+
+    @Test
+    public void onJoinUs() throws Exception {
+
+    }
+
+}
